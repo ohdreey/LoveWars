afde52c1c1e1dfea1d6edc1e2f33fdda55da6550
SummerCoder4 <deoganro81@gmail.com>
2021-12-22 03:49:47 +0100

    made things

--- lovewars/src/App.js
+++ lovewars/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import {Routes, Route } from "react-router-dom"
 import './App.css';
+import Navbar from './Components/Navbar';
+import Chats from './Components/Chats'
+// import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <Header /> */}
+
+    
+        <Routes>          
+               
+           <Route path="/chats" element={<Chats />} /> 
+           <Route path="/logo" element={<App />} />         
+           
+           
+        </Routes>
+        <Navbar />
+    
     </div>
+   
   );
 }
 
---
+++ lovewars/src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import React, {useState,useEffect} from "react";
+import {NavLink} from "react-router-dom"
+import "./Navbar.css";
+
+
+
+
+export default function NavBar(){
+    return (
+      <nav>
+          <div className="liste">
+              
+            <li className="items">Swipez</li>
+            <NavLink to="/App"><li className="items">Logo</li></NavLink>
+            <NavLink to= "/Chats"><li className="items"></li></NavLink>
+            
+          </div>
+      </nav>
+    )
+  }
+
